9bd5dd2a3694300755790a5aa0c6c15c387b5a8e
chenminggui <1728903026@qq.com>
2024-01-19 15:19:54 +0800

    feat: 注释

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,4 +1,3 @@
-/*spring-security*/
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
@@ -7,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Spring Security配置
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
--- src/main/java/com/example/demo/controller/SPAController.java
+++ src/main/java/com/example/demo/controller/SPAController.java
@@ -1,9 +1,11 @@
-/*单页应用控制器*/
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 单页应用控制器
+ */
 @Controller
 public class SPAController {
     @RequestMapping(value = "/{path:[^.]*}")
