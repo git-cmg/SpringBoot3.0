5e48ef0435c3544b847228bfa82431c6c4b2155a
chenminggui <1728903026@qq.com>
2024-01-26 15:13:36 +0800

    build: swagger

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,9 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.LoginDto;
 import com.example.demo.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(tags = "登录")
+@Tag(name = "登录")
 @Data
 @Slf4j
 @RequestMapping("/api")
@@ -22,9 +23,11 @@ public class LoginController {
 
     private final UserService userService;
 
-    @ApiOperation(value = "用户登录")
+    @Operation(summary = "用户登录")
+    @ApiResponse(description = "token")
     @PostMapping("/login")
-    public String login(@ApiParam(name = "用户信息", required = true) @Validated @RequestBody LoginDto loginDto) {
+    public String login(
+            @Parameter(name = "loginDto", description = "用户信息", required = true) @Validated @RequestBody LoginDto loginDto) {
         return userService.login(loginDto.getUsername(), loginDto.getPassword());
     }
 }
