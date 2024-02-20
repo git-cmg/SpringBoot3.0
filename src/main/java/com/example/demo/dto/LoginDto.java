15ed7dd49f3977241bb976563b3ee519a44e1b51
陈明贵 <48546154+git-cmg@users.noreply.github.com>
2024-02-20 16:00:29 +0800

    Update LoginDto.java

--- src/main/java/com/example/demo/dto/LoginDto.java
+++ src/main/java/com/example/demo/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import io.swagger.v3.oas.annotations.dedia.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,11 +10,13 @@ public class LoginDto {
      * 用户名
      */
     @NotNull(message = "用户名不允许为空")
+    @Schema(description = "用户名")
     private String username;
 
     /**
      * 密码
      */
     @NotNull(message = "密码不允许为空")
+    @Schema(description = "密码")
     private String password;
 }
